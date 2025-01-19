1decc8a86e636513a39738d548e72ab17ce09ad6
lucalana <lucalana13@gmail.com>
2025-01-19 18:35:47 -0300

    adicionando formatação para o arquivo excel

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,14 +4,47 @@ namespace App\Exports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class UsersExport implements FromCollection
+class UsersExport implements FromCollection, WithHeadings, ShouldAutoSize, WithStyles, WithMapping
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true]],
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'Nome',
+            'Email',
+            'Data Verificação Do Email',
+            'Data da Criação',
+            'Data de Atualização'
+        ];
+    }
+
     public function collection()
     {
         return User::all();
+    }
+
+    public function map($user): array
+    {
+        return [
+            $user->id,
+            $user->name,
+            $user->email,
+            $user->email_verified_at->format('d/m/Y'),
+            $user->created_at->format('d/m/Y'),
+            $user->updated_at->format('d/m/Y'),
+        ];
     }
 }
